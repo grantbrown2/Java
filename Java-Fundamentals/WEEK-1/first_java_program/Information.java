27dc0ad830cc4df34e74d5d95eb504cf334497cb
grantbrown2 <gbrownzzzz58@gmail.com>
2023-04-04 20:40:40 -0400

    week-1-day-1 added content

---
+++ Java-Fundamentals/WEEK-1/first_java_program/Information.java
@@ -0,0 +1,7 @@
+public class Information {
+    public static void main(String[] args) {
+        System.out.println("My name is Grant Brown");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometown is Hanover, PA");
+    }
+}
